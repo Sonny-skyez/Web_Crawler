dd42ac5ab52727de60a23f0a14c5bd5b7d855059
SonnySkyez <hnic85@gmail.com>
2019-02-11 19:46:56 +0100

    4 commit

--- web_crawler.py
+++ web_crawler.py
@@ -5,6 +5,8 @@ Web Crawler:
 
 This script takes a site URL as an argument
 and creates a map of that domain as a Python dictionary.
+Script will work properly wit exemplary site
+url: http://0.0.0.0:8000
 '''
 
 
@@ -17,7 +19,10 @@ dict = {}
 def site_map(url):
 
     '''This function takes url (http://0.0.0.0:8000) as parameter
-    and creates a python dictionary. Dictionaries '''
+    and creates a python dictionary. Dictionaries contain
+    URL, title and links within every site.'''
+
+    # create response object
     res = requests.get(url)
 
     # check if the page exist
@@ -27,20 +32,19 @@ def site_map(url):
     except Exception as exc:
         print('Error has occured:\t %s' % (exc))
 
-
+    # create Beautiful soup object
     siteSoup = bs4.BeautifulSoup(res.text, features='html.parser')
 
 
-    titleSoup = siteSoup.select('title')
+    titleSoup = siteSoup.select('title')    # search for website title
     title = titleSoup[0].getText()
 
+    linksSoup = siteSoup.find_all('a')      # search for links
 
-    linksSoup = siteSoup.find_all('a')
+    links = set()       # create a set of links
 
-
-    links = set()
-
-    index_url = 'http://0.0.0.0:8000'
+    url_split = url.split('/',3)
+    index_url = url_split[0] + '//' + url_split[2]
 
 
     for item in linksSoup:
@@ -55,6 +59,8 @@ def site_map(url):
 
             links.add(index_url + item.attrs['href'])
 
+        else:
+            continue
 
 
 
@@ -82,8 +88,6 @@ def site_map(url):
     return dict
 
 
-
-
-
 site_map('http://0.0.0.0:8000')
+
 print(dict)
